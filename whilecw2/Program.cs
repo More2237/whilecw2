958a4facd51f0e6a7fce589cd367a72debc271db
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-29 18:06:00 +0500

    gghj

---
+++ whilecw2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace whilecw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 10;
+            
+            int sum = 0;
+            while (x < y)
+            {
+                sum += 1;
+                x++;
+                
+            }
+            Console.WriteLine(sum);   
+        }
+    }
+}
